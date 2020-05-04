5983d34dda269760270ee7fa87dd629271e6c468
akiraozaki1108 <61183197+akiraozaki1108@users.noreply.github.com>
2020-05-04 23:21:13 +0900

    practice4

---
+++ sample04.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<!-- ここにプログラムを記述します -->
+<?php 
+print('PHPを勉強中です');
+?>
+
+<?php $sum = 100*200; ?>
+合憲金額は　<?php print($sum); 円です。?>
+
+消費税込みの値段は <?php print($sum * 1.0); 円です。?>
+
+</pre>
+</main>
+</body>    
+</html>
