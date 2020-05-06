66e4daaccc6361c13b1d252790a59a7ed1ebcfa9
akiraozaki1108 <61183197+akiraozaki1108@users.noreply.github.com>
2020-05-06 23:30:40 +0900

    practice

--- sample05.php
+++ sample05.php
@@ -23,6 +23,16 @@ date_default_timezone_set("Asia/Tokyo");
 $today = new DateTime();
 print($today->format(' G時　iフン　s秒'));
 ?>
+
+<?php 
+$sum = 100+400*4;
+?>
+<?php 
+$tax = 1.08;
+?>
+合計金額は<?php print ($sum); ?>です。
+消費税込みは<?php print($tax*$sum); ?>です。
+
 <!-- ここにプログラムを記述します -->
 </pre>
 </main>
