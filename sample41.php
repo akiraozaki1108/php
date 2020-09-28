dbc3793e5f13740a2e8d293330f614535eabf78f
akiraozaki1108 <61183197+akiraozaki1108@users.noreply.github.com>
2020-09-28 20:08:47 +0900

    Create sample41.php

---
+++ sample41.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>test</h2>
+<pre>
+<!-- ここにプログラムを記述します -->
+    <?php
+    try {
+        $db = new PDO('mysql:dbname=mydb;host=127.0.0.1;charset=utf8', 'root', '');
+        $db ->exec('INSERT INTO memos SET memo="' .$_POST['memo'] . '", created_at=NOW()');
+   } catch(PDOExeption $e) {
+      echo 'DB接続エラー: ' . $e->getmessage();
+
+   }
+   ?>
+   
+</pre>
+</main>
+</body>    
+</html>
